1b9c005e731238db3deb4dbf084f0aba95c9bad6
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-19 13:04:33 +0900

    feat(Input): 사용자 입력 기능 구현

--- src/main/java/calculator/Controller/CalculatorController.java
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -1,13 +1,23 @@
 package calculator.Controller;
 
 import calculator.Constant.MessageConstants;
+import calculator.View.InputView;
 import calculator.View.OutputView;
 
 public class CalculatorController {
     private OutputView outputView = new OutputView();
+    private InputView inputView = new InputView();
 
     public CalculatorController() {
         outputView.printMessage(MessageConstants.CALCULATOR_START_MESSAGE);
+    }
+
+    public void operateCalculator() {
+        String userInput = getUserInput();
+    }
+
+    private String getUserInput() {
+        return inputView.readInput().trim();
     }
 
 }
